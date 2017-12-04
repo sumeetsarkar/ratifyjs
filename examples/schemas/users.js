289b3cebb3a55d4351fb9ab7269960de4fb966d2
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-12-04 18:50:39 +0530

    Exporting validation types from ratify

--- examples/schemas/users.js
+++ examples/schemas/users.js
@@ -1,37 +1,39 @@
+const { Types, } = require('../../lib');
+
 module.exports = {
-  type: 'Array',
+  type: Types.Array,
   length: {
     max: 10,
   },
   default: [],
   children: {
-    type: 'Object',
+    type: Types.Object,
     fields: {
       firstName: {
-        type: 'String',
+        type: Types.String,
         required: true,
         test: /^[a-zA-Z]{2,12}/,
       },
       lastName: {
-        type: 'String',
+        type: Types.String,
         default: '',
       },
       age: {
-        type: 'Number',
+        type: Types.Number,
         required: true,
         range: {
           min: 18,
         },
       },
       languages: {
-        type: 'Array',
+        type: Types.Array,
         length: {
           min: 1,
           max: 5,
         },
         required: true,
         children: {
-          type: 'String',
+          type: Types.String,
         },
       },
     },
